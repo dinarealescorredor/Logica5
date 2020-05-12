aa42e162811e151348c40fd9a0eba305ef31e565
Dina <dinamar582@hotmail.com>
2020-05-11 23:59:50 -0500

    .

--- src/Matriz.java
+++ src/Matriz.java
@@ -324,119 +324,27 @@ public class Matriz {
         this.main();
     }
 
-    /*
-
-
-    private void pintarVector(String msg) {
-        if(this.tam <  20) {
-            String display = msg + "\n" + "";
-            for (int i = 0; i < this.vectorX.length; i++) {
-                display = display + " VectorX[" + (i + 1) + "] =" + this.vectorX[i] + "\n";
+    private void Matriz3{
+        String name1 = "Matriz A\n";
+        String name2 = "Matriz ^3\n";
+        String result = " ";
+        String result1 = " ";
+        for (int i = 0; i < nFilas; i++) {
+            for (int j = 0; j < nCol; j++) {
+                result1 += matrizA[i][j];
+                result1 += "      ";
+                result += Math.pow((matrizA[i][j]),3);
+                result += "      ";
             }
-            JOptionPane.showMessageDialog(null, display, msg, JOptionPane.INFORMATION_MESSAGE);
-        }  else {
-            System.out.println("\n");
-            System.out.println(msg);
-            for (int i = 0; i < vectorX.length; i++) {
-                int pos = i + 1;
-                System.out.println("El vector de la posicion " + pos + " tiene el valor de " + vectorX[i]);
-            }
+            result += "\n";
+            result1 += "\n";
         }
+        result += "\n";
+        result1 += "\n";
+        JOptionPane.showMessageDialog(null, name1 + result1 + name2 + result);
+        this.main();
     }
 
-    private void cambiarElemento() {
-        String position = JOptionPane.showInputDialog("Ingrese el valor de la posición que desea cambiar");
-        if (position == null) {
-            JOptionPane.showMessageDialog(null, "Adios");
-            System.exit(0);
-        }
-        if (validate(position)) {
-            int pos = Integer.parseInt(position);
-            if (pos == 0 || pos > this.tam) {
-                JOptionPane.showMessageDialog(null, "La posición  supera el tamaño del vector");
-                this.cambiarElemento();
-            }
-
-            int valor = this.vectorX[pos - 1];
-            int options = JOptionPane.showOptionDialog(
-                    null,
-                    " En  la posición " + pos + " existe el valor " + valor + " ¿Deseas cambiarlo?",
-                    "Logica",
-                    JOptionPane.YES_NO_CANCEL_OPTION,
-                    JOptionPane.QUESTION_MESSAGE,
-                    null,    // null para icono por defecto.
-                    new Object[]{"Si", "No"},   // null para YES, NO y CANCEL
-                    "Si"
-            );
-
-            if (options == 0) {
-                String value = "";
-                do {
-                    value = JOptionPane.showInputDialog("Ingrese el nuevo número");
-                    if (value == null) {
-                        JOptionPane.showMessageDialog(null, "Adios");
-                        System.exit(0);
-                    }
-                } while (!value.isBlank() && !value.matches("-?\\d+"));
-
-                this.vectorX[pos - 1] = Integer.parseInt(value);
-                JOptionPane.showMessageDialog(null, "El nuevo valor de la posición " + pos + " es " + value);
-            } else if (options == 1) {
-                Random random = new Random();
-                int hasta = 100;
-                int desde = 0;
-
-                int numAleatorio = random.nextInt(hasta - desde + 1) + desde;
-                this.vectorX[pos - 1] = numAleatorio;
-            } else {
-                JOptionPane.showMessageDialog(null, "Por favor seleccione si o no");
-                this.cambiarElemento();
-            }
-        } else {
-            JOptionPane.showMessageDialog(null, "Ingrese un número valido");
-            this.cambiarElemento();
-        }
-        pintarVector("Intercambiar elemento :::");
-    }
-
-    private void intercambiarPos() {
-        String inputA = JOptionPane.showInputDialog("Ingrese el valor de la posición: A");
-        String inputB = JOptionPane.showInputDialog("Ingrese el valor de la posición: B");
-
-        if (validate(inputA) && validate(inputB)) {
-            int x = Integer.parseInt(inputA) - 1;
-            int y = Integer.parseInt(inputB) - 1;
-            if(x == y ){
-                JOptionPane.showMessageDialog(null, "No pueden ser igual las 2 posiciones");
-                this.intercambiarPos();
-            }
-            int posFinal = this.vectorX.length;
-
-            if (x < posFinal && y < posFinal) {
-                int dato;
-                dato = this.vectorX[x];
-                this.vectorX[x] = this.vectorX[y];
-                this.vectorX[y] = dato;
-            } else {
-                JOptionPane.showMessageDialog(null, "Ingrese un número entre  1 y  " + posFinal);
-                this.intercambiarPos();
-            }
-        } else {
-            JOptionPane.showMessageDialog(null, "Ingrese un número valido");
-            this.intercambiarPos();
-        }
-        pintarVector("Intercambiar Posiciones :::");
-    }
-
-    private void inverter(){
-        for(int i=0; i<this.vectorX.length/2; i++){
-            int temp = vectorX[i];
-            vectorX[i] = vectorX[vectorX.length -i -1];
-            vectorX[vectorX.length -i -1] = temp;
-        }
-        pintarVector("Vector invertido :::");
-    }
-    */
 
     private boolean validate(String input) {
         return !input.isBlank() && input.matches("^\\d+$");
